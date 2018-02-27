57a42761e368e9850892613dabae59e662e2763c
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-02-27 14:09:10 -0800

    Added props and css styling to code

--- recipe-props-starter/src/Recipe.js
+++ recipe-props-starter/src/Recipe.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import './Recipe.css';
 
 class Recipe extends Component {
-	render()
+	render() {
+		// shorthand for const title = this.props.title
+		const {title, img, instructions} = this.props;
+		const ingredients = this.props.ingredients.map((ing, index) => (
+			<li key={index}>{ing}</li>
+		));
+		return (
+		<div className='recipe-card'>
+			<div className='recipe-card-img'>
+				<img src={img} alt={title} />
+			</div>
+			<div className='recipe-card-content'>
+				<h3 className='recipe-title'>Recipe {title}</h3>
+				<h4>Ingredients</h4>
+			
+				<ul>
+					{ingredients}
+				</ul>
+				<h4>Instructions</h4>
+				<p> {instructions} </p>
+			</div>
+		</div>
+		);
+	}
 }
 
 export default Recipe;
--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Recipe from './Recipe';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Recipe 
+        title='pasta' 
+        instructions="Mix Ingredients."
+        ingredients={['flour', 'water']} 
+        img='https://images-gmi-pmc.edge-generalmills.com/59d9b710-99b6-41d9-ada8-7d8807da9fca.jpg'/>
       </div>
     );
   }
