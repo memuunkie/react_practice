30e62b486966f599d33cd7fc49f46417c731e75c
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-03-05 15:26:08 -0800

    Added NavBar component and fixed title in Recipe component

--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Recipe from './Recipe';
+import NavBar from './NavBar';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
+      	<NavBar />
         <Recipe 
-        title='pasta' 
+        title='Pasta' 
         instructions="Mix Ingredients."
         ingredients={['flour', 'water']} 
         img='https://images-gmi-pmc.edge-generalmills.com/59d9b710-99b6-41d9-ada8-7d8807da9fca.jpg'/>
