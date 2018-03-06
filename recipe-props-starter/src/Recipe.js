b001f57f6ece8af2aa35312e0426f6b39c706629
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-03-05 16:38:31 -0800

    Added RecipeList component to hold recipe props; edited RecipeApp to use RecipeList and added notes

--- recipe-props-starter/src/Recipe.js
+++ recipe-props-starter/src/Recipe.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Recipe.css';
 
 class Recipe extends Component {
+	// creates a 'template' of what the properties for Recipe should be
+	static propTypes = {
+		title: PropTypes.string.isRequired,
+		ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+		instructions: PropTypes.string.isRequired,
+		img: PropTypes.string.isRequired
+	}
 	render() {
 		// shorthand for const title = this.props.title
 		const {title, img, instructions} = this.props;
@@ -10,10 +18,8 @@ class Recipe extends Component {
 		));
 		return (
 		<div className='recipe-card'>
-			<div className='recipe-card-img'>
-				<img src={img} alt={title} />
-			</div>
 			<div className='recipe-card-content'>
+				<img className='recipe-card-img' src={img} alt={title} />
 				<h3 className='recipe-title'>{title}</h3>
 				<h4>Ingredients</h4>
 			
--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Recipe from './Recipe';
+import RecipeList from './RecipeList';
 import NavBar from './NavBar';
 import './RecipeApp.css';
 
@@ -9,11 +8,7 @@ class RecipeApp extends Component {
     return (
       <div className="App">
       	<NavBar />
-        <Recipe 
-        title='Pasta' 
-        instructions="Mix Ingredients."
-        ingredients={['flour', 'water']} 
-        img='https://images-gmi-pmc.edge-generalmills.com/59d9b710-99b6-41d9-ada8-7d8807da9fca.jpg'/>
+        <RecipeList />
       </div>
     );
   }
