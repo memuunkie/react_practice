a84ba35e990e89cd093f28e53dbe1a125244ecd1
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-03-06 19:12:55 -0800

    Added code to App.js to remove one hobby from list

--- update-complex-state/src/App.js
+++ update-complex-state/src/App.js
@@ -21,6 +21,31 @@ class App extends Component {
         }
       ]
     };
+
+    setTimeout( ()  => {
+      const randInstructor = Math.floor(
+        Math.random() *
+        this.state.instructors.length
+      );
+
+      const hobbyIndex = Math.floor(
+        Math.random() * 
+        this.state.instructors[randInstructor].length
+      );
+      // creates a copy of original state instructors array
+      const instructors = this.state.instructors.slice();
+      // the below code is a no-no.
+      // it's not 'creating a copy'; it's actually changing the state of
+      // the original hobbies array in state.instructors because
+      // it's still referencing the hobbies array, not a copy
+      // instructors[randInstructor].hobbies.splice(hobbyIndex,1)
+      instructors[randInstructor] = Object.assign({}, instructors.[randInstructor]);
+      // above is making a copy of the original object.
+      // below a new array is made from the original state array.
+      instructors[randInstructor].hobbies = instructors[randInstructor].hobbies.slice();
+      instructors[randInstructor].hobbies.splice(hobbyIndex, 1);
+      this.setState({instructors});
+    }, 5000);
   }
   render() {
     const instructors = this.state.instructors.map((instructor, index) => (
