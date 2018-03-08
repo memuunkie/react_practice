22085bd0a9a09534773967d4b1557247c6984c2b
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-03-07 17:11:55 -0800

    Added more notes to App.js re: state

--- update-complex-state/src/App.js
+++ update-complex-state/src/App.js
@@ -3,8 +3,27 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 // creating a "template" for instructors
-// will inherit state from App
-class InstructorItem extends Component {
+// stateless
+
+const InstructorItem = props => {
+  return (
+    <li>
+      <h3>{ props.name }</h3>
+      <h4>
+        Hobbies: { props.hobbies.join(', ') }
+      </h4>
+    </li>
+  );
+}
+
+InstructorItem.propTypes = {
+    name: PropTypes.string,
+    hobbies: PropTypes.arrayOf(PropTypes.string)
+  }
+
+/* this version relies on state inherited from App. Above is stateless.
+
+  class InstructorItem extends Component {
   static propTypes = {
     name: PropTypes.string,
     hobbies: PropTypes.arrayOf(PropTypes.string)
@@ -22,6 +41,7 @@ class InstructorItem extends Component {
   }
 
 }
+*/
 
 class App extends Component {
   constructor(props) {
@@ -67,24 +87,25 @@ class App extends Component {
         }
         return inst;
 
-        // above a refactor of below; doing the same thing
-        // i === randInstructor ? {
-        //   ...inst,
-        //   hobbies: [...inst.hobbies.slice(0, hobbyIndex).concat(inst.hobbies.slice(hobbyIndex + 1, inst.hobbies.length))]
-        // } : inst
-
+      /*  above a refactor of below; doing the same thing
+        i === randInstructor ? {
+          ...inst,
+          hobbies: [...inst.hobbies.slice(0, hobbyIndex).concat(inst.hobbies.slice(hobbyIndex + 1, inst.hobbies.length))]
+        } : inst
+      */
       });
 
-      // the below code is a no-no.
-      // it's not 'creating a copy'; it's actually changing the state of
-      // the original hobbies array in state.instructors because
-      // it's still referencing the hobbies array, not a copy
-      // instructors[randInstructor].hobbies.splice(hobbyIndex,1
-      // instructors[randInstructor] = Object.assign({}, instructors[randInstructor]);
-      // above is making a copy of the original object.
-      // below a new array is made from the original state array.
-      // instructors[randInstructor].hobbies = instructors[randInstructor].hobbies.slice();
-      // instructors[randInstructor].hobbies.splice(hobbyIndex, 1);
+      /*the below code is a no-no.
+      it's not 'creating a copy'; it's actually changing the state of
+      the original hobbies array in state.instructors because
+      it's still referencing the hobbies array, not a copy
+      instructors[randInstructor].hobbies.splice(hobbyIndex,1
+      instructors[randInstructor] = Object.assign({}, instructors[randInstructor]);
+      above is making a copy of the original object.
+      below a new array is made from the original state array.
+      instructors[randInstructor].hobbies = instructors[randInstructor].hobbies.slice();
+      instructors[randInstructor].hobbies.splice(hobbyIndex, 1);
+      */
       this.setState({instructors});
     }, 5000);
   }
@@ -95,12 +116,12 @@ class App extends Component {
         name={instructor.name}
         hobbies={instructor.hobbies}
       />
-    ));
-    //  <li key={index}>
+    /*<li key={index}>
     //    <h3>{instructor.name}</h3>
     //    <h4>Hobbies: {instructor.hobbies.join(", ")}</h4>
     //  </li>
-    //));
+    */
+    ));
     return (
       <div className="App">
         <ul>
