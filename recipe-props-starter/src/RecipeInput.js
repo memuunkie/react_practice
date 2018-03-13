12cbd2200c87d5d03cf61e71bd78ef99b1b2bf0d
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-03-12 19:11:03 -0700

    Created new recipe form. Some positioning issues

--- recipe-props-starter/src/RecipeInput.js
+++ recipe-props-starter/src/RecipeInput.js
@@ -1,10 +1,136 @@
 import React, { Component } from 'react';
+import './RecipeInput.css';
 
 class RecipeInput extends Component {
-	constuctor(props) {
+	static defaultProps = {
+		onClose() {}
+	}
+
+	constructor(props) {
 		super(props);
+		this.state = {
+			title: '',
+			instructions: '',
+			ingredients: [''],
+			img: '',
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleNewIngredient = this.handleNewIngredient.bind(this);
+		this.handleChangeIng = this.handleChangeIng.bind(this);
+	}
+
+	// set this up so it can be used by more than one input
+	handleChange(evt) {
+		this.setState({[evt.target.name]: evt.target.value});
+	}
+
+	handleNewIngredient(evt) {
+		const {ingredients} = this.state;
+		this.setState({ingredients: [...ingredients, '']});
+	}
+
+	handleChangeIng(evt) {
+		const index = Number(evt.target.name.split('-')[1]);
+		const ingredients = this.state.ingredients.map((ing, index) => (
+			index === index ? evt.target.value : ing
+		));
+		this.setState({ingredients});
 	}
 	render() {
-		
+		const {title, instructions, ingredients, img} = this.state;
+		const {onClose} = this.props;
+		let inputs = ingredients.map((ing, index) => (
+			<div
+				className='recipe-form-line'
+				key={`ingredient-${index}`}
+			>
+				<label>{ index + 1 }.
+					<input 
+						type='text'
+						name={ `ingredient-${index}` }
+						value={ ing }
+						size={ 45 }
+						autoComplete='off'
+						placeholder='Ingredient'
+						onChange={ this.handleChangeIng }
+					/>
+				</label>
+			</div>
+		));
+		return (
+			<div className='recipe-form-container'>
+				<form className='recipe-form' onSubmit={ () => {} }>
+					<button
+						type='button'
+						className='close-button'
+						onClick={ onClose }>
+						X
+					</button>
+				
+					<div className='recipe-form-line'>
+						<label htmlFor='recipe-title-input'>Title</label>
+						<input 
+							id='recipe-title-input'
+							key='title'
+							name='title'
+							type='text'
+							value={ title }
+							size={ 42 }
+							autoComplete='off'
+							onChange={ this.handleChange }
+						/>
+						<label 
+							htmlFor='recipe-instructions-input'
+							style={{ marginTop: '5px'}}
+						>
+						Instructions
+						</label>
+						<textarea 
+							key='ingredients'
+							id='recipe-instructions-input'
+							type='instructions'
+							name='instructions'
+							rows='8'
+							cols='50'
+							autoComplete='off'
+							value={ instructions }
+							onChange={ this.handleChange }
+						/>
+						{ inputs }
+						<button
+							type='button'
+							onClick={ this.handleNewIngredient }
+							className='buttons'
+						>
+						+	
+						</button>
+						<div className='recipe-form-line'>
+							<label htmlFor='recipe-img-input'>Image Url</label>
+							<input 
+								id='recipe-img-input'
+								type='text'
+								placeholder=''
+								name='img'
+								value={ img }
+								size={ 36 }
+								autoComplete='off'
+								onChange={ this.handleChange }
+							/>
+						</div>
+						<button
+							type='submit'
+							className='buttons'
+							style={{ alignSelf: 'flex-end', marginRight:0 }}
+						>
+						SAVE	
+						</button>
+					</div>
+				</form>
+			</div>
+		);
+
 	}
 }
+
+export default RecipeInput;
