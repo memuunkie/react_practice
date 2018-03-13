fddb8eef08a0f7226b903561044a1e1e8f49fdca
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-03-12 17:39:49 -0700

    Modified RecipeApp to include ids, and RecipeList to use id as key

--- recipe-props-starter/src/RecipeList.js
+++ recipe-props-starter/src/RecipeList.js
@@ -4,19 +4,14 @@ import PropTypes from 'prop-types';
 import './RecipeList.css';
 
 class RecipeList extends Component {
-	// create a default set of properties for Recipe app
-	static defaultProps = {
-		recipes: [
-		]
-	};
 	static propTypes = {
 		// create an array of the properties for rendering
 		recipes: PropTypes.arrayOf(PropTypes.object).isRequired
 	};
 	render() {
 		// map the recipes array to create element
-		const recipes = this.props.recipes.map((recipe, index) => (
-			<Recipe key={index} {...recipe} />
+		const recipes = this.props.recipes.map((recipe, id) => (
+			<Recipe key={ recipe.id } { ...recipe } />
 		));
 		return (
 			<div className='recipe-list'>
