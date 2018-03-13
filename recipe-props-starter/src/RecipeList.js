4af4f7f08011a4d895ecadbb1f184241b6f08806
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-03-12 17:30:28 -0700

    Added state and default props to RecipeApp

--- recipe-props-starter/src/RecipeList.js
+++ recipe-props-starter/src/RecipeList.js
@@ -7,24 +7,6 @@ class RecipeList extends Component {
 	// create a default set of properties for Recipe app
 	static defaultProps = {
 		recipes: [
-			{
-				title: 'Spaghetti',
-				instructions: 'Open jar of spaghetti sauce. Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce.',
-		        ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-		        img: "spaghetti.jpg"
-      		},
-	      	{
-	    		title: "Milkshake",
-	        	instructions: "Combine ice cream and milk.  Blend until creamy",
-	        	ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
-	        	img: "milkshake.jpg"
-	      	},
-		    {
-		        title: "Avocado Toast",
-		        instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
-		        ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
-		        img: "avocado_toast.jpg"
-		    }
 		]
 	};
 	static propTypes = {
@@ -38,7 +20,7 @@ class RecipeList extends Component {
 		));
 		return (
 			<div className='recipe-list'>
-				{recipes}
+				{ recipes }
 			</div>
 			);
 	}
