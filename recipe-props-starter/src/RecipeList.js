a9224cbcdb4ca9b10334e0c1ee0ac5c75a3e6206
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-03-13 16:31:40 -0700

    Added delete functionality on recipe tiles

--- recipe-props-starter/src/RecipeList.js
+++ recipe-props-starter/src/RecipeList.js
@@ -6,12 +6,14 @@ import './RecipeList.css';
 class RecipeList extends Component {
 	static propTypes = {
 		// create an array of the properties for rendering
-		recipes: PropTypes.arrayOf(PropTypes.object).isRequired
+		recipes: PropTypes.arrayOf(PropTypes.object).isRequired,
+		onDelete: PropTypes.func.isRequired,
 	};
 	render() {
+		const {onDelete} = this.props;
 		// map the recipes array to create element
 		const recipes = this.props.recipes.map((recipe, id) => (
-			<Recipe key={ recipe.id } { ...recipe } />
+			<Recipe key={ recipe.id } { ...recipe } onDelete={ onDelete }/>
 		));
 		return (
 			<div className='recipe-list'>
