f653f784a312bf182c91df292246b8bfc90fa7e0
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-04-07 14:19:10 -0700

    Added notes to code

--- hacker_news/src/App.js
+++ hacker_news/src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// trying out componentDidMount
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: []
+    };
+  }
+
+  componentDidMount() {
+    const topStories = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+    const storyUrlBase = 'https://hacker-news.firebaseio.com/v0/item/';
+
+    fetch(topStories)
+    // more promise chaining...
+    // get the json
+    // map the json then fetch all the urls
+    // set the state
+      .then(data => data.json())
+      .then(data => data.map(id => {
+        const url = `${storyUrlBase}${id}.json`;
+        return fetch(url).then(d => d.json());
+      }))
+      .then(promises => Promise.all(promises))
+      .then(stories => this.setState({stories}));
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    let views = <div>Loading...</div>
+    const {stories} = this.state;
+    // keeps the 'Loading...' on screen while componentDidMount does its thing
+    // checks if anything is returned and formats it
+    if (stories && stories.length > 0) {
+      views = stories.map(s => (
+        <p key={ s.id }>
+          <a href={ s.url }>{ s.title }</a> from <strong>{ s.by }</strong> 
         </p>
+      ));
+    }
+
+
+    return (
+      <div className='App'>
+        <h2>Hacker News Top Stories</h2>
+        { views }
       </div>
     );
   }
+
 }
 
 export default App;
